c8e370feef3fb5e791cf98d26a3529bb45503212
szymanskir <ryszard.szymanski@outlook.com>
2021-02-02 20:01:29 +0100

    Apply styler

--- tests/testthat/test-helpers.R
+++ tests/testthat/test-helpers.R
@@ -1,7 +1,9 @@
 test_that("if wrapper evaluates the expression when the feature flag is enabled", {
   enabled_flag <- create_bool_feature_flag(value = TRUE)
 
-  result <- feature_if(enabled_flag, {2 * 7})
+  result <- feature_if(enabled_flag, {
+    2 * 7
+  })
 
   expect_equal(result, 14)
 })
@@ -10,7 +12,9 @@ test_that("if wrapper evaluates the expression when the feature flag is enabled"
 test_that("if wrapper ommits the evaluation of the expression when the feature flag is disabled", {
   disabled_flag <- create_bool_feature_flag(value = FALSE)
 
-  result <- feature_if(disabled_flag, {2 * 7})
+  result <- feature_if(disabled_flag, {
+    2 * 7
+  })
 
   expect_equal(result, NULL)
 })
@@ -21,8 +25,12 @@ test_that("ifelse wrapper runs the left expression when flag is enabled", {
 
   result <- feature_ifelse(
     enabled_flag,
-    {1 + 1},
-    {1 * 1}
+    {
+      1 + 1
+    },
+    {
+      1 * 1
+    }
   )
 
   expect_equal(result, 2)
@@ -34,8 +42,12 @@ test_that("ifelse wrapper runs the right expression when flag is disabled", {
 
   result <- feature_ifelse(
     disabled_flag,
-    {1 + 1},
-    {1 * 1}
+    {
+      1 + 1
+    },
+    {
+      1 * 1
+    }
   )
 
   expect_equal(result, 1)
