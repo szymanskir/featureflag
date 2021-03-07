53358d1d04c7adb10cc271904791bcec279f9df8
szymanskir <ryszard.szymanski@outlook.com>
2021-03-07 18:21:41 +0100

    Fix lintr violations

--- tests/testthat/test-feature_flag-time_period.R
+++ tests/testthat/test-feature_flag-time_period.R
@@ -3,7 +3,9 @@ test_that("Time period feature flags are enabled within specified boundaries", {
     from = ISOdatetime(2020, 1, 1, 10, 0, 0, tz = "UTC"),
     to = ISOdatetime(2020, 1, 1, 13, 0, 0, tz = "UTC")
   )
-  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", function() ISOdatetime(2020, 1, 1, 12, 0, 0, tz = "UTC"))
+
+  sys_time_stub <- function() ISOdatetime(2020, 1, 1, 12, 0, 0, tz = "UTC")
+  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", sys_time_stub)
 
   expect_true(is_enabled(feature_flag))
 })
@@ -14,7 +16,9 @@ test_that("Time period feature flags are disabled when not in specified boundari
     from = ISOdatetime(2020, 1, 1, 10, 0, 0, tz = "UTC"),
     to = ISOdatetime(2020, 1, 1, 13, 0, 0, tz = "UTC")
   )
-  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", function() ISOdatetime(2020, 1, 1, 15, 0, 0, tz = "UTC"))
+
+  sys_time_stub <- function() ISOdatetime(2020, 1, 1, 15, 0, 0, tz = "UTC")
+  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", sys_time_stub)
 
   expect_false(is_enabled(feature_flag))
 })
@@ -23,7 +27,9 @@ test_that("Time period feature flags bounded from are enabled from specified bou
   feature_flag <- create_time_period_feature_flag(
     from = ISOdatetime(2020, 1, 1, 10, 0, 0, tz = "UTC")
   )
-  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", function() ISOdatetime(2920, 1, 1, 15, 0, 0, tz = "UTC"))
+
+  sys_time_stub <- function() ISOdatetime(2920, 1, 1, 15, 0, 0, tz = "UTC")
+  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", sys_time_stub)
 
   expect_true(is_enabled(feature_flag))
 })
@@ -33,7 +39,9 @@ test_that("Time period feature flags bounded from are disabled to specified boun
   feature_flag <- create_time_period_feature_flag(
     from = ISOdatetime(2020, 1, 1, 10, 0, 0, tz = "UTC")
   )
-  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", function() ISOdatetime(2020, 1, 1, 9, 59, 59, tz = "UTC"))
+
+  sys_time_stub <- function() ISOdatetime(2020, 1, 1, 9, 59, 59, tz = "UTC")
+  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", sys_time_stub)
 
   expect_false(is_enabled(feature_flag))
 })
@@ -43,7 +51,9 @@ test_that("Time period feature flags bounded `to` are enabled until specified bo
   feature_flag <- create_time_period_feature_flag(
     to = ISOdatetime(2020, 1, 1, 10, 0, 0, tz = "UTC")
   )
-  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", function() ISOdatetime(1990, 1, 1, 9, 0, 0, tz = "UTC"))
+
+  sys_time_stub <- function() ISOdatetime(1990, 1, 1, 9, 0, 0, tz = "UTC")
+  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", sys_time_stub)
 
   expect_true(is_enabled(feature_flag))
 })
@@ -53,7 +63,9 @@ test_that("Time period feature flags bounded to are disabled from specified boun
   feature_flag <- create_time_period_feature_flag(
     to = ISOdatetime(2020, 1, 1, 10, 0, 0, tz = "UTC")
   )
-  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", function() ISOdatetime(2020, 1, 1, 10, 59, 59, tz = "UTC"))
+
+  sys_time_stub <- function() ISOdatetime(2020, 1, 1, 10, 59, 59, tz = "UTC")
+  mockery::stub(is_enabled.time_period_feature_flag, "Sys.time", sys_time_stub)
 
   expect_false(is_enabled(feature_flag))
 })
